07b96069c9457dbdd27634a5fd144a813d4c685a
devdynam0507 <wsnam0507@naver.com>
2020-03-24 06:24:32 +0000

    add server sock

---
+++ src/server/handler/HttpClientConnectHandler.java
@@ -0,0 +1,18 @@
+package server.handler;
+
+import java.net.Socket;
+
+public class HttpClientConnectHandler extends EndyHttpAsyncEvent<Socket> {
+    
+    public static HttpClientConnectHandler instance(Socket client) { return new HttpClientConnectHandler(client); }
+    
+    private HttpClientConnectHandler(Socket client) {
+        super(client);
+    }
+    
+    public void handle(Socket client) {
+        BufferedReader reader = new BufferedReader(new InputStream(client.getInputStream()));
+        System.out.println(reader.readLine());
+    }
+    
+}
